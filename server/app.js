8e6e2687cc6e582e45da7f42d76a173fe5471b8f
Dennis <dennisostendorf@web.de>
2019-11-23 12:48:58 +0100

    Defined GraphQL schema and imported it to app

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
 
 const app = express()
 
 app.use('/api', graphqlHTTP({
-
+  schema: schema // import schema to middleware
 }))
 
 app.listen(4000, () => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,7 @@
 const graphql = require('graphql')
 
-// ES6 destructuring: retrieve and assign GraphQLObjectType function from graphql
-const { GraphQLObjectType, GraphQLString } = graphql
+// ES6 destructuring: retrieve and assign necessary properties from graphql
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
 
 // define what a 'Book' should look like
 const BookType = new GraphQLObjectType({
@@ -11,4 +11,24 @@ const BookType = new GraphQLObjectType({
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
   })
+})
+
+// RootQuery defines how the frontend can 'jump into' the graph
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType, // define that querying 'book' will return a GraphQLObject of BookType
+      args: { id: { type: GraphQLString } }, // define that a query for a book expects also the ID of the book as an argument
+      resolve(parent, args) {
+        // here data from a database or other source can be retrieved
+        args.id
+      }
+    }
+  }
+})
+
+// export which query a user can use from the frontend (schema)
+module.exports = new GraphQLSchema({
+  query: RootQuery
 })
