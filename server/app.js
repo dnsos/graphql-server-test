111ea3c7423dee378d535e6b38e9d496ffb8af04
Dennis <dennisostendorf@web.de>
2019-11-24 19:03:52 +0100

    Connected to Mongo database

--- server/app.js
+++ server/app.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+
+const creds = require('./creds')
+const { DB_URL } = creds
 
 const app = express()
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+mongoose.connection.once('open', () => {
+  console.log('Connected to database')
+})
 
 // middleware
 app.use('/api', graphqlHTTP({
